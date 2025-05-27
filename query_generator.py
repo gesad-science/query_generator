a9cf035221cdec9094afbb8dcb4e84724a07c426
Joaquim Ribeiro <joaquimgamer23@gmail.com>
2025-05-27 15:50:54 -0300

    modifying propmts and altering code structure

--- query_generator.py
+++ query_generator.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import CSVLoader
 from langchain_text_splitters import TokenTextSplitter
 import numpy as np
+import pandas as pd
 import random
 import requests
 import json
@@ -52,7 +53,7 @@ Rewritten Input:
 evolution_templates = [multi_context_template, reasoning_template, hypothetical_scenario_template]
 
 #Function to acess models thorugh chat
-def ollama_chat(prompt, model="llama3"):
+def ollama_chat(prompt, model="mistral"):
     url = "http://localhost:11434/api/generate"
     response = requests.post(url, json={
         "model":model,
@@ -82,68 +83,99 @@ def evolve_query(original_input, context, steps):
         current_input = ollama_chat(evolved_prompt)
     return current_input
 
-#Chunk Document
+def context_generation(input_path):
+    #Chunk Document
+    text_splitter = TokenTextSplitter(chunk_size=1024, chunk_overlap=0)
+    loader = CSVLoader(input_path, encoding="utf-8")
+    raw_chunks = loader.load_and_split(text_splitter)
+
+    #Convert chunks into embeddings
+    content = [rc.page_content for rc in raw_chunks]
+    embeddings = ollama_embeddings(content)
+
+    #Select a random chunk to use as reference
+    reference_index = random.randint(0, len(embeddings) - 1)
+    reference_embedding = embeddings[reference_index]
+    contexts = [content[reference_index]]
+
+    #Identify similarity
+    similarity_threshold = 0.8
+    similar_indices = []
+    for i, embedding in enumerate(embeddings):
+        product = np.dot(reference_embedding, embedding)
+        norm = np.linalg.norm(reference_embedding) * np.linalg.norm(embedding)
+        similarity = product / norm
+        if similarity>= similarity_threshold and i != reference_index:
+            similar_indices.append(i)
+
+    for i in similar_indices:
+        contexts.append(content[i])
+
+    print(contexts)
+    return contexts
+
+def generate_queries(input_path, opc, contexts):
+    #Query Generation
+    prompt = ""
+    if(opc == 3):
+        prompt = f"""Bellow there is contexts embeddings, which were generated splitting a dataset of commands sent from user to a chatbot that stores and edits a database, and gruouping the chunks of this dataset throught simillarity on embeddings:
+
+        {contexts}
+
+        Based on this contexts, generate more 200 simillar commands, whit variations in names, actions, intents and structures, keep in mind the quantity requested.
+        """
+
+    if(opc == 2):
+        with open(input_path, 'r', encoding='utf-8') as file:
+            dataset = file.read()
+        
+        prompt = f"""Abaixo estão exemplos de comandos enviados por usuários a um chatbot de que armazena e edita base de dados conforme o usuário pede:
+
+        {dataset}
+
+        Com base nos exemplos, gere mais 200 comandos semelhantes, com variações nos nomes, ações e estruturas, podendo ser de diferentes domínios.
+        """
+
+    query = ollama_chat(prompt)
+    print(query)
+    return query
+
+def evolve_queries(query):
+    parsed_queries = json.loads(query)
+    evolved_queries = []
+    um_evolution_steps = 1 #KEEP IT 1
+
+    for item in parsed_queries:
+        original_input = item["input"]
+        evolved = evolve_query(original_input, contexts, num_evolution_steps)
+        print(evolved)
+        evolved_queries.append({"original": original_input, "evolved": evolved})
+
+    #Export to a CSV file
+    output_path = "Results/generated_queries.csv"
+    with open(output_path, mode="w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=["original", "evolved"])
+        writer.writeheader()
+        for row in evolved_queries:
+            writer.writerow(row)
+
+    print(f"\nAs queries foram salvas em: {output_path}")
+
+
+print("========= Query Generator 1.0 =========")
+print("\n1) Gerar contextos com os embeddings.")
+print("2) Gerar queries a partir de CSV (sem contextos).")
+print("3) Gerar queries a partir dos contextos.")
+print("4) Evoluir queries geradas.")
+print("5) Fazer contextos, geração e evolução completas.\n")
+rsp = int(input("Digite operação: "))
+
 input_path = "Input/dataset_tratado.csv"
-text_splitter = TokenTextSplitter(chunk_size=1024, chunk_overlap=0)
-loader = CSVLoader(input_path, encoding="utf-8")
-raw_chunks = loader.load_and_split(text_splitter)
 
-#Convert chunks into embeddings
-content = [rc.page_content for rc in raw_chunks]
-embeddings = ollama_embeddings(content)
-
-#Select a random chunk to use as reference
-reference_index = random.randint(0, len(embeddings) - 1)
-reference_embedding = embeddings[reference_index]
-contexts = [content[reference_index]]
-
-#Identify similarity
-similarity_threshold = 0.8
-similar_indices = []
-for i, embedding in enumerate(embeddings):
-    product = np.dot(reference_embedding, embedding)
-    norm = np.linalg.norm(reference_embedding) * np.linalg.norm(embedding)
-    similarity = product / norm
-    if similarity>= similarity_threshold and i != reference_index:
-        similar_indices.append(i)
-
-for i in similar_indices:
-    contexts.append(content[i])
-
-print(contexts)
-
-#Query Generation
-prompt = f"""I want you to act as a dataset generator, given the context below, which is a list of strings, generate a list of 100 JSON objects, each object must have only one key: "input".
-
-The value of "input" should be a realistic question or statement that could be answered or addressed using the context provided.
-
-Important:
-- Only return a valid JSON list of 100 objects.
-- Each object must look like this: {{ "input": "your question or statement here" }}
-
-Context:
-{contexts}"""
-
-query = ollama_chat(prompt)
-print(query)
-
-#Evolve Queries
-parsed_queries = json.loads(query)
-evolved_queries = []
-num_evolution_steps = 1 #KEEP IT 1
-
-for item in parsed_queries:
-    original_input = item["input"]
-    evolved = evolve_query(original_input, contexts, num_evolution_steps)
-    print(evolved)
-    evolved_queries.append({"original": original_input, "evolved": evolved})
-
-#Export to a CSV file
-output_path = "Results/generated_queries.csv"
-with open(output_path, mode="w", newline="", encoding="utf-8") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=["original", "evolved"])
-    writer.writeheader()
-    for row in evolved_queries:
-        writer.writerow(row)
-
-print(f"\nAs queries foram salvas em: {output_path}")
+if(rsp == 1):
+    context_generation(input_path)
+if(rsp == 2):
+    generate_queries(input_path, rsp)
+if(rsp == 3):
+    context = context_generation(input_path)
+    generate_queries(input_path, rsp, context)
