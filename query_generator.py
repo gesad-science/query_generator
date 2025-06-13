b2cc8f618c16267b57a934b52d49f21fff637844
Joaquim Ribeiro <joaquimgamer23@gmail.com>
2025-06-13 10:53:29 -0300

    completed pipeline

--- query_generator.py
+++ query_generator.py
@@ -19,7 +19,9 @@ openai.api_key = os.getenv("OPEN_AI_KEY")
 
 # Correction of caracther problems
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-spinner_generation = Halo(text='Gerando Queries\n', spinner='dots')
+spinner_generation = Halo(text='Generating Queries\n', spinner='dots')
+spinner_contexts = Halo(text='Generating Contexts\n', spinner='dots')
+spinner_evolution = Halo(text='Evolving Queries\n', spinner='dots')
 
 # Evolution prompt templpates
 multi_context_template = """
@@ -71,7 +73,7 @@ evolution_templates = [
 ]
 
 # Acess models thorugh chat using ollama
-def ollama_chat(prompt, model="llama3:70b"):
+def ollama_chat(prompt, model="gemma3:27b"):
     url = "http://localhost:11434/api/generate"
     response = requests.post(url, json={
         "model":model,
@@ -119,7 +121,9 @@ def context_generation(input_path):
 
     # Convert chunks into embeddings
     content = [rc.page_content for rc in raw_chunks]
+    spinner_contexts.start()
     embeddings = ollama_embeddings(content)
+    spinner_contexts.stop()
 
     # Select a random chunk to use as reference
     reference_index = random.randint(0, len(embeddings) - 1)
@@ -127,13 +131,15 @@ def context_generation(input_path):
     contexts = [content[reference_index]]
 
     # Identify similarity
+    # TODO: TEST WITH DIFFERENT VALUES
     similarity_threshold = 0.8
+    
     similar_indices = []
     for i, embedding in enumerate(embeddings):
         product = np.dot(reference_embedding, embedding)
         norm = np.linalg.norm(reference_embedding) * np.linalg.norm(embedding)
         similarity = product / norm
-        if similarity>= similarity_threshold and i != reference_index:
+        if similarity >= similarity_threshold and i != reference_index:
             similar_indices.append(i)
 
     for i in similar_indices:
@@ -150,10 +156,12 @@ def context_generation(input_path):
     return contexts
 
 # Function to peform query generation using contexts or file examples
-def generate_queries(opc, input_path="", context=""):
+def generate_queries(opc, input_path, context=""):
     # Query Generation
     prompt = ""
-    if opc == 3:
+    i = 42
+    if opc == 3 or opc == 5:
+        #TODO: Adjust the prompt
         prompt = f"""Bellow there is contexts embeddings, which were generated 
         splitting a dataset of commands sent from user to a chatbot that stores 
         and edits a database, and gruouping the chunks of this dataset throught 
@@ -167,7 +175,6 @@ def generate_queries(opc, input_path="", context=""):
         """
 
     if opc == 2:
-        i = 36
         cont = 0
         #while cont < 5:
         if cont == 0:
@@ -176,67 +183,18 @@ def generate_queries(opc, input_path="", context=""):
         else:
             with open(
                 f"Results/generated_queries{i}.csv",
-                'r', encoding='utf-8') as file:
-                dataset = file.read()
+                'r', encoding='utf-8'
+                ) as file:
+                    dataset = file.read()
         
         cont += 1
 
-        prompt2 = f"""Abaixo segue um dataset onde a coluna user_msg contem as
-        mensagens de um usário para um chatbot, a coluna expected_intent 
-        contem a intenção do usuário, a coluna expect_attributes contem os 
-        atributos da mensagem, a coluna expected_filter_attributes contem os 
-        filtros das operações de update, read e delete. 
-
-        {dataset}
-
-        Com base nesses dados gere 50 linhas de mensagens com a classificação 
-        de seus respectivos elementos de acorodo com as colunas do csv.
-
-        """
-
-        prompt = f"""Abaixo estão exemplos em um arquivo csv de comandos
-        enviados por usuários a um chatbot de que armazena e edita base de
-        dados conforme  o usuário pede através de linguagem natural, sem
-        utilizar termos técnicos e em inglês, onde a coluna user_msg
-        representa a mensagem de um usuário para um chatbot,
-        a expected_intent representa a intenção que o usuário tem
-        (ADD, READ, DELETE, UPDATE), a expected_class representa a entidade
-        ou tabela que o usuário que realizar uma operação, a
-        expected_attributes representa os atributos da mensagem e o
-        expected_filter_attributes representa os filtros de operações
-        update, read e delete, ambos devem ser uma string de JSON como será
-        mostrado no dataset:
-
-        {dataset}
-
-        Com base nos exemplos, gere um arquivo csv com 200 comandos gerados a 
-        partir dos exemplos, com variações nos nomes, operações, podendo ser
-        diferentes domínios e assuntos, mas nenhum comando pode ser igual ao 
-        outro ou aos comandos nos exemplos, você deve entregar todos os dados 
-        gerados, sem abreviações por meio de "..." ou "Aqui estão os 200 
-        comandos que você pediu: ...", deve conter exatamente a quantidade 
-        exigida de 200, os comandos não devem possuir numeração antes dele, 
-        você deve entregar apenas os comandos no formato de um cvs, sem nenhum
-        texto adicional ou expliocativo, exclusivamente os comados.
-        """
-        prompt1 = f"""Bellow are interactions of users with a chatbot, where the
-        user_msg column represents the message from the user to the chatbot,
-        expected_intent is the intention of the user (ADD, READ, DELETE, UPDATE)
-        , expected_class is the entity that the user wants to realize a
-        operation, expected_attributes is the attributes of the message and 
-        expected_filtter_attributes is the filtters of the update, read and 
-        delete operations, both should be a JSON string
-
-        {dataset}
-
-        Using the examples, generate a CSV file with 200 interactions, with 
-        variations in the names and operations, using different or the same 
-        domain as the example, but any interaction should be equal to another or 
-        equal to the examples.
+        prompt = f"""
+        
         """
 
     spinner_generation.start()
-    response = ollama_chat(prompt3)
+    response = ollama_chat(prompt)
     #response = openai_chat(prompt)
     print(response)
     spinner_generation.stop()
@@ -253,18 +211,23 @@ def generate_queries(opc, input_path="", context=""):
     return output_path
 
 def evolve_queries(contexts, query_output_path):
-    parsed_queries = json.loads(query)
+    with open(query_output_path, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        generated_queries = [row for row in reader]
+    
     evolved_queries = []
     num_evolution_steps = 3
 
-    for item in parsed_queries:
-        original_input = item["input"]
+    spinner_evolution.start()
+    for item in generated_queries:
+        original_input = item.get("response") or item.get("input") or ""
         evolved = evolve_query(original_input, contexts, num_evolution_steps)
         print(evolved)
         evolved_queries.append({"original": original_input, "evolved": evolved})
+    spinner_evolution.stop()
 
     #Export to a CSV file
-    output_path = "Results/generated_queries.csv"
+    output_path = "Results/evolved_queries.csv"
     with open(output_path, mode="w", newline="", encoding="utf-8") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=["original", "evolved"])
         writer.writeheader()
@@ -290,9 +253,10 @@ if(rsp == 2):
     generate_queries(rsp, input_path)
 if(rsp == 3):
     context = context_generation(input_path)
-    generate_queries(rsp, context)
+    generate_queries(rsp, input_path, context)
 if(rsp == 4):
     evolve_queries()
 if(rsp == 5):
     context = context_generation(input_path)
-    generate_queries(rsp, context)
+    result_path = generate_queries(rsp, input_path, context)
+    evolve_queries(context, result_path)
